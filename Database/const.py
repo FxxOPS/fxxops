937329da40225c5b14ff3c895832285abf5c56d3
abbottdba <root@harrison-vm-01.(none)>
2015-09-22 20:57:32 -0700

    db config

--- Database/const.py
+++ Database/const.py
@@ -2,9 +2,9 @@
 '''
 @author: yuan.gao6
 '''
-DB_ADDRESS = 'localhost'
+DB_ADDRESS = '10.1.110.24'
 DB_PORT = 3306
-DB_USER = ''
-DB_PWD = ''
+DB_USER = 'root'
+DB_PWD = '123456'
 DB_CHAR_SET = 'utf8'
-DB_DEF = ''
+DB_DEF = 'ops'
