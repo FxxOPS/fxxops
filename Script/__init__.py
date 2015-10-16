68488fd26c064c297f70cf27d860f21985dabfab
abbottdba <root@harrison-vm-01.(none)>
2015-10-15 18:42:11 -0700

    V1.3 mysql

--- Script/__init__.py
+++ Script/__init__.py
@@ -1,4 +1,8 @@
 # -*- coding: utf-8 -*-
 __author__ = 'Abbott'
 
-from . import mysqlconf
+# from . import mysqlconf
+import logging
+logger = logging.getLogger("ServerListLogger")
+
+from . import auto_dig
